a6b732f66f866a79653badc1bbacaf5f20d170ed
Bryan Failach <142352242+bfailach@users.noreply.github.com>
2024-11-26 22:00:17 -0500

    Update ConexionBD.java

--- ConexionBD.java
+++ ConexionBD.java
@@ -7,12 +7,10 @@ public class ConexionBD {
     private static final String USER = "root";  
     private static final String PASSWORD = "";  
 
-    // Establecer conexión con la base de datos
     public static Connection getConexion() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
-    // Cerrar la conexión con la base de datos
     public static void cerrarConexion(Connection conn) throws SQLException {
         if (conn != null) {
             conn.close();
